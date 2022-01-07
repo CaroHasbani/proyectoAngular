33321b4052d86518ab094de39d346ac4925c64db
CarolinaHasbani <hasbanicarolina@gmail.com>
2022-01-07 09:35:25 -0300

    subscription

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { MovieAPI } from 'src/app/models/movieAPI.models';
 import { CartService } from 'src/app/services/cart.service';
@@ -12,13 +13,13 @@ import { environment } from 'src/environments/environment';
 })
 export class CartComponent implements OnInit {
 
-
+private subscription= new Subscription;
 public list: MovieAPI[] = [];
 
   constructor(private cartService: CartService) { }
   urlPath = environment.imgAPI;
   ngOnInit(): void {
-    this.cartService.getList().subscribe(response => this.list = response);
+    this.subscription.add(this.cartService.getList().subscribe(response => this.list = response));
   }
 
   remove(id:number){
@@ -26,6 +27,10 @@ public list: MovieAPI[] = [];
       console.log( response)
       this.cartService.getList().subscribe(response => this.list = response);
     });
+  }
+  ngOnDestroy():void{
+    // para que se borren los datos al salir
+    this.subscription.unsubscribe();
   }
 
   // clearCart(){
